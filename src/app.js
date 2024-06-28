107081993d37368766d4e34ec2fed535489cdea7
Christian David Villar Colodro <86432506+ChristianDVillar@users.noreply.github.com>
2024-06-28 20:54:46 +0000

    [Proyecto de la clase] Se creo la carta con las indicaciones de la clase src/index:Se genero la carta src/app.js: se realizo el random src/style.css: se hizo el style de la carta

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function generarcarta() {
+    // Listas de opciones para construir la excusa
+    let palo = ["♠", "♥", "♦", "♣"];
+    let valorCarta = [
+      "AS",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "7",
+      "9",
+      "10",
+      "King",
+      "Queen"
+    ];
+    let eleccionPalo = palo[Math.floor(Math.random() * palo.length)];
+    let eleccionValor =
+      valorCarta[Math.floor(Math.random() * valorCarta.length)];
+    let paloMostrar = `${eleccionPalo}`;
+    let valorMostrar = `${eleccionValor}`;
+    // Mostrar la carta
+    let paloMostarArriba = document.getElementById("mostarArriba");
+    let paloMostarAbajo = document.getElementById("mostarAbajo");
+    let valor = document.getElementById("valor");
+    paloMostarArriba.innerText = paloMostrar;
+    paloMostarAbajo.innerText = paloMostrar;
+    valor.innerText = valorMostrar;
+
+    if (paloMostrar == "♥" || paloMostrar == "♦") {
+      document.getElementById("mostarArriba").style.color = "red";
+      document.getElementById("valor").style.color = "red";
+      document.getElementById("mostarAbajo").style.color = "red";
+    }
+  }
+  generarcarta();g
 };
